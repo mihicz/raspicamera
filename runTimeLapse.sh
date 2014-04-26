81b6bb2378da74ee8f6873e20485f33f8dd4e1b0
raspi <mihicz@users.noreply.github.com>
2014-04-26 14:05:27 +0200

    call 'date' from /bin/

--- runTimeLapse.sh
+++ runTimeLapse.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#TODO: unalias date
-
 if [ -z "$1" -o "$1" = " " ]; then
 	echo "Input value is null or space!"
 	exit 1
@@ -9,10 +7,10 @@ fi
 
 # calculate end time
 #END_TIME="2014-03-03 21:06:00"
-#END_TIME=`date +%Y-%m-%d`\ "18:00:00"
-END_TIME=`date +%Y-%m-%d`\ "$1"
+#END_TIME=`/bin/date +%Y-%m-%d`\ "18:00:00"
+END_TIME=`/bin/date +%Y-%m-%d`\ "$1"
 # get seconds since epoch
-t1=`date --date="$END_TIME" +%s` || {
+t1=`/bin/date --date="$END_TIME" +%s` || {
 	echo "Wrong input value!"
 	exit 1
 }
@@ -21,7 +19,7 @@ echo $t1
 
 
 
-DATE=$(date -I)
+DATE=$(/bin/date -I)
 DIRECTORY=/home/pi/camera/timelapse_$DATE
 #DIRECTORY=/home/pi/temp/timelapse_$DATE
 FORMAT=jpg #png,bmp,jpg,gif
@@ -35,21 +33,22 @@ fi
 # max 2592x1944 px
 WIDTH=750
 HEIGHT=750
-TIMEOUT=500 # in ms, must be integer and > 0
-SETTINGS="-ev -2 -awb auto" # try raw without awb?
+TIMEOUT=1000 # in ms, must be integer and > 0
+#SETTINGS="-ev -2 -awb auto" # try raw without awb?
+SETTINGS="-awb auto"
 
 # read exif information
 IMAGE_TEXT="%[EXIF:DateTimeOriginal]"
 
 # delay time
-TIMELAPSE=10s
+TIMELAPSE=7s
 
 
 while true; do
 
 	# end condition
-	CUR_TIME=`date +%Y-%m-%d\ %H:%M:%S`
-	t2=`date --date="$CUR_TIME" +%s`
+	CUR_TIME=`/bin/date +%Y-%m-%d\ %H:%M:%S`
+	t2=`/bin/date --date="$CUR_TIME" +%s`
 	echo $CUR_TIME
 	echo $t2
 	if [ $t2 -ge $t1 ]; then
@@ -75,6 +74,6 @@ while true; do
 	sleep $TIMELAPSE;
 done
 
-avconv -r 10 -i $DIRECTORY/Image_%04d.$FORMAT -r 10 -vcodec libx264 -crf 20 -g 15 $DIRECTORY/video_$DATE.mp4
+avconv -r 60 -i $DIRECTORY/Image_%04d.$FORMAT -r 60 -vcodec libx264 -crf 20 -g 15 $DIRECTORY/video_$DATE.mp4
 
 echo "done"
